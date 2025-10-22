212abc279a85975beaa240b97e2e36be25d8e2e0
clover475 <lmnc475@gmail.com>
2025-10-22 10:07:08 +0800

    Update builder.ts

--- lib/builder.ts
+++ lib/builder.ts
@@ -1,4 +1,4 @@
 // lib/builder.ts
 import { builder } from '@builder.io/react';
-builder.init('xxxx'); // 96be0094500d4631a116768018bd9514
+builder.init('xxxx'); // 9910cc50fa434a8ca7e4f21e539f3333
 export { builder };
