357bcbfe9b7fb27f0de3a43b29410a29ed2679a7
clover475 <lmnc475@gmail.com>
2025-10-22 10:13:05 +0800

    Reorder import statements in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { builder } from '@/lib/builder';
 import { BuilderComponent } from '@builder.io/react';
+import { builder } from '@/lib/builder';
 
 export default async function Home() {
   const content = await builder
     .get('page', { userAttributes: { urlPath: '/' } })
     .toPromise();
-
   return <BuilderComponent model="page" content={content} />;
 }
